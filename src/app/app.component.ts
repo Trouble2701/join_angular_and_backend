41463660ef507360126d5204dca44184a239e8b9
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2025-05-24 10:14:06 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SitebarComponent } from './shared/sitebar/sitebar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SitebarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/shared/sitebar/sitebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sitebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sitebar.component.html',
+  styleUrl: './sitebar.component.scss'
+})
+export class SitebarComponent {
+
+}
